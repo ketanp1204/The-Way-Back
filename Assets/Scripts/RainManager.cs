d9e9241b374111d9ac7d0dc2ba5c02ed4ea5cbeb
ketanp1204 <ketan.patel@uni-weimar.de>
2020-07-27 12:33:37 +0200

    Added rain to more windows and fixed the rain stopping logic when the time of day goes into noon.

---
+++ Assets/Scripts/RainManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainManager : MonoBehaviour
+{
+    void Start()
+    {
+        if(GameSession.currentTimeOfDay != GameSession.TimeOfDay.MORNING)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            StartCoroutine(ShowRainAnimationTillNoon());
+        }
+    }
+
+    private IEnumerator ShowRainAnimationTillNoon()
+    {
+        gameObject.SetActive(true);
+
+        while (!GameSession.instructionsSeen)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(GameSession.timeOfDayInterval - GameSession.gameTime);
+        
+        gameObject.SetActive(false);
+    }
+}
